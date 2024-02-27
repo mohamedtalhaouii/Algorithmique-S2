1f5bff7aa6c47f7afd495b04fd5418d9c8133b1a
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2024-02-27 15:03:39 +0100

    add comment

--- TP/TP1/EX2-Q3.py
+++ TP/TP1/EX2-Q3.py
@@ -4,6 +4,7 @@ for i in range(1,6):
     #Demande
     N = float(input("entrer la note de la matier " + str(i) + " : " ))
     C = int(input("Son Coefficeint ? : "))
+    #Calcule la somme des notes et des coefficients
     S = S + (N * C)
     SC = SC + C
 
